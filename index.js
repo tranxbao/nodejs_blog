f5329641f18916de9a517ce3f640347596b836f8
Bao Tran <tranxbao@gmail.com>
2022-03-02 18:06:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//route.
+
+app.get('/tin-tuc', (req, res) => {
+    
+  res.send('Hello World!')
+})
+//127.0.0.1 -localhost:3000
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
